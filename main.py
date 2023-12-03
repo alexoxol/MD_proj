4a08cad1348667c7d51339f6b7dd0dd3fa7c3a0d
alexoxol78 <alexoxol78@ddd.com>
2023-12-03 14:22:33 +0300

    tictactoe again 2

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ class GameWindow(Screen, MDFloatLayout):
     cells_x = [False for _ in range(9)]
     cells_o = [False for _ in range(9)]
     curr_cells = cells_x
-    game_in_process = False
     btns = []
     myLabel = None
     # Выигрышные комбинации
@@ -33,13 +32,13 @@ class GameWindow(Screen, MDFloatLayout):
 
     def on_pre_enter(self, *args):
         # Открыто окно. Рестарт или продолжение?
-        conn = sqlite3.connect("titactoe.db")
-        cur = conn.cursor()
-        cur.execute("""
+        records = self.parent.db_select("""
             SELECT * FROM cur_game
         """)
-        records = cur.fetchall()
-        conn.close()
+        # Удаляем из БД
+        self.parent.db_exec("""
+            DELETE FROM cur_game
+        """)
         self.restart()
         if len(records) == 0:
             self.restart()
@@ -53,7 +52,6 @@ class GameWindow(Screen, MDFloatLayout):
         self.cells_x = eval(rec[1])
         self.cells_o = eval(rec[2])
         self.curr_cells = self.cells_x if self.turn == "X" else self.cells_o
-        self.game_in_process = False
         for i, btn in enumerate(self.btns):
             # Красим доску
             if self.cells_x[i]:
@@ -103,27 +101,37 @@ class GameWindow(Screen, MDFloatLayout):
         self.curr_cells = self.cells_x if self.turn == "X" else self.cells_o
         # Проставляем в этот блок True
         self.curr_cells[btn.tag] = True
+        # Удаляем из БД
+        self.parent.db_exec("""
+            DELETE FROM cur_game
+        """)
         # Провереям на победу
         win_index = self.checkForWin(self.curr_cells)
         if win_index >= 0:
             # Есть победитель
-            self.game_in_process = False
             self.myLabel.text = f"Победитель: {self.turn}"
             # Затемняем все кнопки
             for btn in self.btns:
                 btn.disabled = True
         elif win_index == -2:
             # Ничья
-            self.game_in_process = False
             self.myLabel.text = f"Ничья!"
             # Затемняем все кнопки
             for btn in self.btns:
                 btn.disabled = True
         else:
             # Нет победителя
-            self.game_in_process = True
             self.turn = "X" if self.turn == "O" else "O"
             self.myLabel.text = f"Ходит: {self.turn}"
+            # Сохраняем в БД
+            conn = sqlite3.connect("titactoe.db")
+            cur = conn.cursor()
+            turn = self.turn
+            cells_x = str(self.cells_x)
+            cells_o = str(self.cells_o)
+            self.parent.db_exec(f"""
+                INSERT into cur_game(turn, cells_x, cells_o) VALUES ('{turn}', '{cells_x}', '{cells_o}')
+            """)
 
     def checkForWin(self, cells):
         for i, win in enumerate(self.wins):
@@ -142,7 +150,6 @@ class GameWindow(Screen, MDFloatLayout):
         self.cells_x = [False for _ in range(9)]
         self.cells_o = [False for _ in range(9)]
         self.curr_cells = self.cells_x
-        self.game_in_process = False
         for btn in self.btns:
             btn.text = ""
             btn.disabled = False
@@ -165,13 +172,11 @@ class MainWindow(Screen, MDFloatLayout):
         btnStart.bind(on_press = self.start)
         self.add_widget(btnStart)
 
-        conn = sqlite3.connect("titactoe.db")
-        cur = conn.cursor()
-        cur.execute("""
+    def on_pre_enter(self, *args):
+        # Добавляем кнопку "Продолжить" (в on_pre_enter, потому что в __init__ не видно self.parent)
+        records = self.parent.db_select("""
             SELECT * FROM cur_game
         """)
-        records = cur.fetchall()
-        conn.close()
         if len(records) > 0:
             btnCont = MDRaisedButton(text="Продолжить")
             btnCont.font_size = "26sp"
@@ -182,78 +187,48 @@ class MainWindow(Screen, MDFloatLayout):
 
     def start(self, btn):
         # Очищаем БД
-        conn = sqlite3.connect("titactoe.db")
-        cur = conn.cursor()
-        cur.execute(f"""
+        self.parent.db_exec("""
             DELETE FROM cur_game
         """)
-        # Commit changes
-        conn.commit()
-        conn.close()
         self.parent.current = "game"
 
     def cont(self, btn):
-        conn = sqlite3.connect("titactoe.db")
-        cur = conn.cursor()
-        cur.execute("""
-            SELECT * FROM cur_game
-        """)
-        records = cur.fetchall()
-        conn.close()
         self.parent.current = "game"
 
 class WindowManager(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.add_widget(MainWindow())
-        self.add_widget(GameWindow())
-        self.current = "main"
-
-class MyApp(MDApp):
-    winman = None
-    def build(self):
-        conn = sqlite3.connect("titactoe.db")
-        cur = conn.cursor()
-        cur.execute("""
+        # Создаем таблицу
+        self.db_exec("""
             CREATE TABLE if not exists cur_game(
                 turn text,
                 cells_x text,
                 cells_o text
             )
         """)
+        self.add_widget(MainWindow())
+        self.add_widget(GameWindow())
+        self.current = "main"
+
+    def db_exec(self, sql):
+        conn = sqlite3.connect("titactoe.db")
+        cur = conn.cursor()
+        cur.execute(sql)
         conn.commit()
         conn.close()
+
+    def db_select(self, sql):
+        conn = sqlite3.connect("titactoe.db")
+        cur = conn.cursor()
+        cur.execute(sql)
+        res = cur.fetchall()
+        conn.close()
+        return res
+
+class MyApp(MDApp):
+    winman = None
+    def build(self):
         self.winman = WindowManager()
         return self.winman
-
-    def on_start(self):
-        pass
-
-    def on_stop(self):
-        # Сохраняем в БД, если игра идет
-        if self.winman.current == "game":
-            conn = sqlite3.connect("titactoe.db")
-            cur = conn.cursor()
-            if self.winman.current_screen.game_in_process:
-                # Игра в процессе. Сохраним
-                turn = self.winman.current_screen.turn
-                cells_x = str(self.winman.current_screen.cells_x)
-                cells_o = str(self.winman.current_screen.cells_o)
-
-                # Сначала очищаем старые записи
-                cur.execute(f"""
-                    DELETE FROM cur_game
-                """)
-                cur.execute(f"""
-                    INSERT into cur_game(turn, cells_x, cells_o) VALUES ('{turn}', '{cells_x}', '{cells_o}')
-                """)
-            else:
-                # Игра завершена. Удалим старые записи
-                cur.execute(f"""
-                    DELETE FROM cur_game
-                """)
-            # Коммит, клоуз
-            conn.commit()
-            conn.close()
 
 MyApp().run()
